86c5506a5546e6c05e580172a675988a31b85951
Alberto Romero <aromeronavia@gmail.com>
2022-01-27 11:33:23 -0600

    Add github star button

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,17 +1,16 @@
 <template>
   <div class="flex justify-between w-screen h-16 bg-navbar py-4 px-6">
     <p class="text-lg text-black font-bold">Piano Keyboard</p>
-    <a href="https://github.com/aromeronavia/piano" target="_blank">
-      <img
-        class="inline mr-4"
-        width="24"
-        height="24"
-        src="~/assets/github.svg"
-        alt="Github Portfolio"
-      />
-    </a>
+    <github-button class="mr-4" href="https://github.com/aromeronavia/piano" data-color-scheme="no-preference: dark; light: light; dark: dark;" data-icon="octicon-star" data-size="large" data-show-count="true" aria-label="Star aromeronavia/piano on GitHub">Star</github-button>
   </div>
 </template>
 
 <script>
+import GithubButton from 'vue-github-button'
+
+export default {
+  components: {
+    GithubButton
+  },
+}
 </script>
